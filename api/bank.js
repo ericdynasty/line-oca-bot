d032d34fb6731f6ebf2f1f38c00db9cd8bfc8390
ericdynasty <eric.dynasty@gmail.com>
2025-09-02 11:59:43 +0800

    Create bank.js

---
+++ api/bank.js
@@ -0,0 +1,75 @@
+// api/bank.js
+// 教材單點句庫（請把教材原文貼進來覆蓋陣列內容即可）
+
+module.exports = {
+  A: { // A 穩定
+    highHeavy: ["【教材】A 高(重) 句1", "【教材】A 高(重) 句2"],
+    highLight: ["【教材】A 高(輕) 句1", "【教材】A 高(輕) 句2"],
+    mid:       ["【教材】A 中性 句1"],
+    lowLight:  ["【教材】A 低(輕) 句1", "【教材】A 低(輕) 句2"],
+    lowHeavy:  ["【教材】A 低(重) 句1", "【教材】A 低(重) 句2"],
+  },
+  B: { // B 欣快
+    highHeavy: ["【教材】B 高(重) 句1"],
+    highLight: ["【教材】B 高(輕) 句1"],
+    mid:       ["【教材】B 中性 句1"],
+    lowLight:  ["【教材】B 低(輕) 句1"],
+    lowHeavy:  ["【教材】B 低(重) 句1"],
+  },
+  C: { // C 堅定
+    highHeavy: ["【教材】C 高(重) 句1"],
+    highLight: ["【教材】C 高(輕) 句1"],
+    mid:       ["【教材】C 中性 句1"],
+    lowLight:  ["【教材】C 低(輕) 句1"],
+    lowHeavy:  ["【教材】C 低(重) 句1"],
+  },
+  D: { // D 確定
+    highHeavy: ["【教材】D 高(重) 句1"],
+    highLight: ["【教材】D 高(輕) 句1"],
+    mid:       ["【教材】D 中性 句1"],
+    lowLight:  ["【教材】D 低(輕) 句1"],
+    lowHeavy:  ["【教材】D 低(重) 句1"],
+  },
+  E: { // E 活躍
+    highHeavy: ["【教材】E 高(重) 句1"],
+    highLight: ["【教材】E 高(輕) 句1"],
+    mid:       ["【教材】E 中性 句1"],
+    lowLight:  ["【教材】E 低(輕) 句1"],
+    lowHeavy:  ["【教材】E 低(重) 句1"],
+  },
+  F: { // F 積極
+    highHeavy: ["【教材】F 高(重) 句1"],
+    highLight: ["【教材】F 高(輕) 句1"],
+    mid:       ["【教材】F 中性 句1"],
+    lowLight:  ["【教材】F 低(輕) 句1"],
+    lowHeavy:  ["【教材】F 低(重) 句1"],
+  },
+  G: { // G 責任
+    highHeavy: ["【教材】G 高(重) 句1"],
+    highLight: ["【教材】G 高(輕) 句1"],
+    mid:       ["【教材】G 中性 句1"],
+    lowLight:  ["【教材】G 低(輕) 句1"],
+    lowHeavy:  ["【教材】G 低(重) 句1"],
+  },
+  H: { // H 評估能力
+    highHeavy: ["【教材】H 高(重) 句1"],
+    highLight: ["【教材】H 高(輕) 句1"],
+    mid:       ["【教材】H 中性 句1"],
+    lowLight:  ["【教材】H 低(輕) 句1"],
+    lowHeavy:  ["【教材】H 低(重) 句1"],
+  },
+  I: { // I 欣賞能力
+    highHeavy: ["【教材】I 高(重) 句1"],
+    highLight: ["【教材】I 高(輕) 句1"],
+    mid:       ["【教材】I 中性 句1"],
+    lowLight:  ["【教材】I 低(輕) 句1"],
+    lowHeavy:  ["【教材】I 低(重) 句1"],
+  },
+  J: { // J 溝通能力
+    highHeavy: ["【教材】J 高(重) 句1"],
+    highLight: ["【教材】J 高(輕) 句1"],
+    mid:       ["【教材】J 中性 句1"],
+    lowLight:  ["【教材】J 低(輕) 句1"],
+    lowHeavy:  ["【教材】J 低(重) 句1"],
+  },
+};
